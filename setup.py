98a379879389c9e9557927836eb1f25d0be9b766
Yura Avdeev <freeyura78@gmail.com>
2018-09-14 13:59:29 +0300

    изменил setup .py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from os.path import join, dirname
 
 setup(
     name='allWords',
+    entry_points={'console_scripts': [
+        'allwords = allwords.wordstat:get_arg',
+    ]},
     version='0.1',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
